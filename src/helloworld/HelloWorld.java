696adb54fdae4427e6599b88cf9ba303426923d2
g12943rk <g12943rk@s30418.tsuda.ac.jp>
2014-04-11 11:03:41 +0900

    新規にHelloWorldを作成。

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g12943rk
+ */
+public class HelloWorld {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("HelloWorld");
+    }
+}
