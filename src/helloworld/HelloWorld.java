88e96188c3d5c659b2ebec50c011e5daa5472bf5
g12926kc <g12926kc@s30417.tsuda.ac.jp>
2014-04-11 11:41:22 +0900

    新規にHelloWorldを作成しました Hello Kiyopiを追加しました

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("Hello Kiyopi");
     }
 }
